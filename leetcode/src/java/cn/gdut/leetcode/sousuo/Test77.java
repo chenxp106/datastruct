5b4e7cf93f51b70f5adb4757c0837ca2b2f82566
chenxiaoping <chenxp106@163.com>
2019-07-31 10:56:19 +0800

    组合求和

--- leetcode/src/java/cn/gdut/leetcode/sousuo/Test77.java
+++ leetcode/src/java/cn/gdut/leetcode/sousuo/Test77.java
@@ -7,23 +7,24 @@ import java.util.List;
 public class Test77 {
     public List<List<Integer>> combine(int n, int k){
         List<List<Integer>> ret = new ArrayList<>();
-
-        List<Integer> curRet = new LinkedList<>();
-        tracking(ret,curRet,1,k,n);
+        List<Integer> curRet = new ArrayList<>();
+        tracking(ret, curRet, 1,k,n);
         return ret;
+
     }
 
-    private void tracking(List<List<Integer>> ret, List<Integer> curRet,int start,int k, int n){
+    private void tracking(List<List<Integer>> ret,List<Integer> curRet, int start, int k, int n){
         if (k == 0){
             ret.add(new ArrayList<>(curRet));
             return;
         }
-        for (int i = start;i<=n - k + 1;i++){
+        for (int i = start;i<=n -k + 1;i++){
             curRet.add(i);
-            tracking(ret,curRet,i + 1,k - 1,n);
+            tracking(ret,curRet, i+1, k-1,n);
             curRet.remove(curRet.size() - 1);
         }
     }
+
 
     public static void main(String[] args) {
         Test77 test77 = new Test77();
