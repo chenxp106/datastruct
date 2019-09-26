95c3076928e3f4dcbfb6edb199c0e90bf48b060f
chenxiaoping <chenxp106@163.com>
2019-09-26 10:56:51 +0800

    把数字翻译成字符串

---
+++ leetcode/src/java/cn/gdut/jianzhi/Test46.java
@@ -0,0 +1,20 @@
+package cn.gdut.jianzhi;
+
+public class Test46 {
+    public int numDecoding(String s){
+//        if (s == null || s.length() == 0){
+//            return 0;
+//        }
+//        int n = s.length();
+//        int [] dp = new int[n+1];
+//        dp[0] = 1;
+//        dp[1] = s.charAt(0) == '0' ? 0:1;
+//        for (int i = 2;i<=n;i++){
+//            int one = Integer.valueOf(s.substring(i-1,i));
+//            if (one != 0){
+//                dp[i]
+//            }
+//        }
+        return 0;
+    }
+}
